bebd6467288af235480fb6657b7be88f9327c348
Arif Rawf <armanbdd@gmail.com>
2026-09-17 07:26:46 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const http = require('http');
 const router = require('./core/routes/router');
-const port = process.env.PORT || 3000;
 
-	
 const server = http.createServer((req, res) => {
 	//Starting router
 	if (router(req,res)) return;
 	}
 );
 
-server.listen(port,() => {
-	console.log('server running on port ' + port);
-	});
+server.listen(
+    process.env.PORT || 3000,
+    "0.0.0.0",
+    () => {
+        console.log("Server running");
+    }
+);
